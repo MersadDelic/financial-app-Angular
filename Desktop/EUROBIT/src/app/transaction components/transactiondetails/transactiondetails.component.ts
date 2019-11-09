5baeb4f5dfcfa52102e3bef5f77dcb2d2e562061
Mersad <talibulilm12@gmail.com>
2019-11-10 00:37:10 +0100

    budgetDetails: bug fix

--- Desktop/EUROBIT/src/app/transaction components/transactiondetails/transactiondetails.component.ts
+++ Desktop/EUROBIT/src/app/transaction components/transactiondetails/transactiondetails.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Transaction} from '../../models/transaction';
 import {Budget} from '../../models/budget';
 import {ActivatedRoute} from '@angular/router';
 import {TransactionService} from '../../services/transaction.service';
+import {BudgetService} from '../../services/budget.service';
 
 @Component({
   selector: 'app-transactiondetails',
@@ -12,8 +13,11 @@ import {TransactionService} from '../../services/transaction.service';
 export class TransactiondetailsComponent implements OnInit {
 
   transaction: Transaction;
+  budget: Budget;
   constructor(private route: ActivatedRoute,
-              private transactionService: TransactionService) { }
+              private transactionService: TransactionService,
+              private budgetService: BudgetService) {
+  }
 
   ngOnInit() {
     this.getmyTransaction();
@@ -21,7 +25,19 @@ export class TransactiondetailsComponent implements OnInit {
 
   getmyTransaction() {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.transactionService.getTransaction(id).subscribe(transaction => this.transaction = transaction);
+    this.transactionService.getTransaction(id)
+      .subscribe(transaction => {
+          this.transaction = transaction;
+          this.getTransactionBudget();
+        },
+        error => console.error(error)
+      );
+  }
 
+  getTransactionBudget() {
+    this.budgetService.getBudget(this.transaction.budget).subscribe(
+      data => this.budget = data,
+      error => console.log(error)
+    );
   }
 }
